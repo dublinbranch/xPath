f0c83adca24d7604df272263e0d30f6989de5e49
claudio <claudio@tech.techadsmedia.com>
2021-06-16 16:11:57 +0100

    removed shadowing

--- xml.cpp
+++ xml.cpp
@@ -206,8 +206,8 @@ QByteArray XmlNode::searchLeaf(const char* path) const {
 		if (nodes->nodeNr == 0) {
 			return res;
 		}
-		auto node = nodes->nodeTab[0];
-		auto vv   = xmlNodeGetContent(node);
+		auto _node = nodes->nodeTab[0];
+		auto vv    = xmlNodeGetContent(_node);
 		if (vv != nullptr) {
 			res.setRawData((const char*)vv, strlen((const char*)vv));
 		}
